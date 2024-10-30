9aab4ba8187c915425095da68e273543272c84e7
Ceri-10 <alberto.canovas@hotmail.com>
2024-10-30 10:57:32 +0100

    Api & coins

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-
-import './App.css'
+import { useEffect } from "react"
+import CriptoSearchForm from "./components/CriptoSearchForm"
+import { useCrytoStore } from "./store"
 
 function App() {
 
+  const fetchCryptos = useCrytoStore((state) =>state.fetchCrytos)
+
+  useEffect(() =>{
+    fetchCryptos()
+  },[])
+
   return (
     <>
+      <div className='container'>
+        <h1 className='app-title'>Cotizador de <span>criptomonedas</span></h1>
+
+        <div className='content'>
+          <CriptoSearchForm />
+
+        </div>
+      </div>
     </>
   )
 }
